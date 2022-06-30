1caeeac745ab566bd1e3c02ca16cdb283a58dcc6
Febrian Amir <febrianamir5@gmail.com>
2022-06-30 17:14:06 +0000

    util: response

---
+++ helper/response.go
@@ -0,0 +1,58 @@
+package helper
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type response struct {
+	Code   int    `json:"code"`
+	Status string `json:"status"`
+	Data   any    `json:"data,omitempty"`
+	Error  string `json:"error,omitempty"`
+}
+
+func SendSuccess(c *gin.Context, data any) {
+	c.JSON(http.StatusOK, response{
+		Code:   200,
+		Status: "SUCCESS",
+		Data:   data,
+	})
+}
+
+func SendBadRequest(c *gin.Context, err error) {
+	c.JSON(http.StatusBadRequest, response{
+		Code:   400,
+		Status: "BAD REQUEST",
+		Error:  err.Error(),
+	})
+	c.Abort()
+}
+
+func SendServerError(c *gin.Context, err error) {
+	c.JSON(http.StatusInternalServerError, response{
+		Code:   500,
+		Status: "SERVER ERROR",
+		Error:  err.Error(),
+	})
+	c.Abort()
+}
+
+func SendUnauthorized(c *gin.Context, err error) {
+	c.JSON(http.StatusUnauthorized, response{
+		Code:   401,
+		Status: "UNAUTHORIZED",
+		Error:  err.Error(),
+	})
+	c.Abort()
+}
+
+func SendNotFound(c *gin.Context, err error) {
+	c.JSON(http.StatusNotFound, response{
+		Code:   404,
+		Status: "NOT FOUND",
+		Error:  err.Error(),
+	})
+	c.Abort()
+}
